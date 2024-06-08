d50efdeb4d57c5df66fb4652c38b1d1cc5bd6f27
VTiw25 <vaibhavtiwari1410@gmail.com>
2024-06-08 22:47:08 +0530

    oil_data.py

--- Assignment_week_3/oil_data.py
+++ Assignment_week_3/oil_data.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read data from CSV file
 data = pd.read_csv('oil.csv')
 
-# Convert 'date' column to datetime format for better handling in plots
 data['date'] = pd.to_datetime(data['date'])
 
-# Plotting the data
 plt.figure(figsize=(12, 6))
 plt.plot(data['date'], data['dcoilwtico'], label='Oil Price')
 plt.xlabel('Date')
